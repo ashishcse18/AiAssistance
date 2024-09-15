786ddb192479923edfbf8657db6c379a99690f38
ashishcse18 <ashishcse18@gmail.com>
2024-09-15 14:54:33 +0530

    Final commit

---
+++ app/src/main/java/com/ashishyaduvanshi/aiassistance/ChatViewModel.kt
@@ -0,0 +1,50 @@
+package com.ashishyaduvanshi.aiassistance
+
+import android.os.Message
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.launch
+
+class ChatViewModel : ViewModel(){
+
+    val messageList by lazy{
+        mutableStateListOf<MessageModel>()
+    }
+
+    val generativeModel : GenerativeModel = GenerativeModel(
+        modelName = "gemini-pro",
+        apiKey = Constants.apiKey
+    )
+    fun sendmessage(question : String){
+        viewModelScope.launch {
+
+            try{
+                val chat = generativeModel.startChat(
+                    history = messageList.map {
+                        content(it.role){text(it.message)}
+                    }.toList()
+
+                )
+
+                messageList.add(MessageModel(question, "user"))
+                messageList.add(MessageModel("typing...", "model"))
+
+                val response = chat.sendMessage(question)
+                messageList.removeLast()
+                messageList.add(MessageModel(response.text.toString(),"model"))
+            }catch (e:Exception){
+                messageList.removeLast()
+                messageList.add(MessageModel("Error : " +e.message.toString(), "model"))
+
+
+        }
+
+            }
+
+
+    }
+}
---
+++ app/src/main/java/com/ashishyaduvanshi/aiassistance/Constants.kt
@@ -0,0 +1,6 @@
+package com.ashishyaduvanshi.aiassistance
+
+object Constants {
+    val apiKey = "AIzaSyCQo96J3lqE7uwINm1Ohkty81cZzkV4nMA "
+
+}
